5536e1f9f05a989cd6fb78c7898d38ba95eccc21
Allen Zhang <azhang9328@gmail.com>
2019-11-17 06:57:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,7 +44,7 @@ def naming_system
   earth = {
     :label => "Kingdom",
     :sub_category => {
-      :label => "Phylum"
+      :label => "Phylum",
       :sub_category => {
         :label => "Class"
       }
